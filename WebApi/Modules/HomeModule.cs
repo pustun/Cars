6fc1ac47918e778fe9789b7e733ab1addfe36de9
Taran Oleksandr <pustunt@gmail.com>
2017-09-17 13:49:57 +0200

    Added index.html page that shows readme.md

---
+++ WebApi/Modules/HomeModule.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+using Nancy;
+
+namespace WebApi.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => Response.AsFile("Static/index.html");
+
+            Get["/readme.md"] = _ => Response.AsFile("bin/readme.md");
+        }
+    }
+}
